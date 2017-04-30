b6007c377fd1e217fa64dbbbf9f91dd014de7090
mgi166 <skskoari@gmail.com>
2017-04-30 11:42:41 +0900

    Add ShowKeyNotification stack.

--- src/views/index.js
+++ src/views/index.js
@@ -1,9 +1,45 @@
 import React, { Component } from 'react';
+import { OrderedSet } from 'immutable';
+import { NotificationStack } from 'react-notification';
 
 export default class ShowKeyNotification extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      notifications: OrderedSet()
+    };
+  }
+
+  addKeyboardEvent(event) {
+    const { notifications } = this.state;
+    const newNotifications = notifications.add(
+      {
+        message: event.key,
+        key: "!!!",
+      }
+    );
+    return this.setState(
+      { notifications: newNotifications }
+    );
+  }
+
+  componentWillUnmount() {
+    console.log("unmount");
+  }
+
   render() {
     return(
-      <div id="test-show-key">
+      <div id="show-key-test">
+        <NotificationStack
+          isActive="true"
+          message="!!!!!"
+          notifications={this.state.notifications.toArray()}
+          onDismiss={
+            notification => this.setState(
+              { notifications: this.state.Notification.delete(notification) }
+            )
+          }
+        />
       </div>
     );
   }
