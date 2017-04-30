548d047ff6e6f4ad741ecae3c5e8208ac06968d7
mgi166 <skskoari@gmail.com>
2017-04-30 17:37:54 +0900

    Load script tag after root element loaded.

---
+++ src/views/options.js
@@ -0,0 +1,5 @@
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+import OptionsForm from './optionsForm';
+
+render(<OptionsForm />, document.querySelector("#options"));
