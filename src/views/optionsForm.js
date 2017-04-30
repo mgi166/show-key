00ce6b6e4136d5178814f78426f9bf8de3279143
mgi166 <skskoari@gmail.com>
2017-04-30 19:55:02 +0900

    Add onSubmit callback function

--- src/views/optionsForm.js
+++ src/views/optionsForm.js
@@ -1,9 +1,20 @@
 import React, { Component } from 'react';
 
 export default class OptionsForm extends Component {
+  onSubmit(e) {
+    e.preventDefault();
+    const target = e.target;
+
+    chrome.storage.sync.set(
+      {
+        eventName: target.eventName.value
+      }
+    );
+  }
+
   render() {
     return (
-      <form>
+      <form onSubmit={this.onSubmit}>
         <label>EventName</label>
         <button type="submit" />
       </form>
