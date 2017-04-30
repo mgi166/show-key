1c2de82b1e7544a8028f21763a6beb7d48c48465
mgi166 <skskoari@gmail.com>
2017-04-30 17:37:38 +0900

    Add form component

---
+++ src/views/optionsForm.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export default class OptionsForm extends Component {
+  render() {
+    return (
+      <form>
+        <label>EventName</label>
+        <button type="submit" />
+      </form>
+    );
+  }
+};
