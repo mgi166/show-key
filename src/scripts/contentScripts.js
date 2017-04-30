8536a5ee3c30c356faea54cc58dd4d1e0f923e42
mgi166 <skskoari@gmail.com>
2017-04-30 11:47:13 +0900

    Call addKeyboardEvent function in keydown event callback
    * This is not working
    
    ```
    Warning: setState(...): Can only update a mounted or mounting component. This usually means you called setState() on an unmounted component. This is a no-op. Please check the code for the ShowKeyNotification component.
    printWarning @ contentScripts.bundle.js:296
    warning @ contentScripts.bundle.js:320
    warnNoop @ contentScripts.bundle.js:1508
    enqueueSetState @ contentScripts.bundle.js:1581
    ReactComponent.setState @ contentScripts.bundle.js:1088
    addKeyboardEvent @ contentScripts.bundle.js:4462
    (anonymous) @ contentScripts.bundle.js:4407
    ```

--- src/scripts/contentScripts.js
+++ src/scripts/contentScripts.js
@@ -5,6 +5,13 @@ import Notification from "../views/index.js";
 const identifier = "show-key";
 const element = document.createElement('div');
 
+window.ShowKeyNotification = Notification;
+
 element.id = identifier;
 document.body.appendChild(element);
 render(<Notification />, document.getElementById(identifier));
+
+document.addEventListener("keydown", (event) => {
+  const notification = new window.ShowKeyNotification();
+  notification.addKeyboardEvent(event);
+});
