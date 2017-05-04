16091232039f37cfcd0c482a60ad9a62e1286a3c
mgi166 <skskoari@gmail.com>
2017-05-04 23:19:08 +0900

    Load from config

--- src/scripts/contentScripts.js
+++ src/scripts/contentScripts.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import { render } from 'react-dom';
 import Notification from "../views/notification";
+import Config from './config';
 
 const identifier = "show-key";
 const element = document.createElement('div');
 
 element.id = identifier;
 document.body.appendChild(element);
-render(<Notification />, document.getElementById(identifier));
+
+const config = new Config;
+
+config.load().then((config) => {
+  render(<Notification config={config} />, document.getElementById(identifier));
+});
